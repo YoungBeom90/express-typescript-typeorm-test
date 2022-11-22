a57cacaaf2d130a1192d57a6a0f23798dc72c165
woody <ybkim@enerdot.co.kr>
2022-11-22 11:00:02 +0900

    port config update.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import {AppDataSource} from "./data-source";
 import route from "./route";
 
+// database init
 AppDataSource.initialize()
     .then(async () => {
         console.log("Connection initialized with database...");
@@ -10,16 +11,15 @@ AppDataSource.initialize()
     .catch((error) => console.log(error));
 
 const app = express();
+app.set('port', 3000);
+app.listen(app.get('port'));
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+//router
 app.use(route);
 
-
 app.get('/', (req, res) => {
-    return res.send('Hello express');
-});
-
-app.listen(3000, () => {
-    console.log('hello port')
+    return res.send('Welcome to my express service main page!');
 });
